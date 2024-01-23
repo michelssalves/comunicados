e882b72dc4b4c8f06893a87344a84e96e15b0453
Michel Alves <82483090+michelssalves@users.noreply.github.com>
2024-01-23 10:06:43 -0300

    finalizado comunicados

--- verComunicado.php
+++ verComunicado.php
@@ -14,9 +14,15 @@ include 'includes/menu-cliente.php';
                 <img style="width: 30px;height: 30px;border-radius: 45%;" src="./assets/img/fotos-perfil/logoRdp.png" />
                 <span class="fidi-date">Publicado em <?= $objeto->dma($row['data_noticia']) ?></span>
                 <hr>
-                <a href=""><img src='./assets/img/imagens/<?= $row['imagem_inicio'] ?>' class='rounded-top col-md-8 d-flex mx-auto'></a>
+                <a href="<?=$row['fonte']?>"><img src='./assets/img/imagens/<?= $row['imagem_inicio'] ?>' class='rounded-top col-md-8 d-flex mx-auto'></a>
+                <div class="container">
+                    <div class="row">
+                    <p><?= htmlspecialchars_decode($row['texto']) ?></p>
+                    <div>
+                </div>
             </div>
         </div>
     </div>
+    <?= $row['texto_capa'] ?>
 </body>
 </html>
